c0ef1301f2476fc30236326af9781aec1e7e3a70
Angelo Tata <atata@ebay.com>
2014-06-08 14:18:00 +0200

    Implement notification retrieval based on username in mentions

--- routes/sendNotifications.js
+++ routes/sendNotifications.js
@@ -2,8 +2,17 @@ var storage;
 
 // Default  notification handler
 function handleNotifications(req, res, next) {
-    res.send(200, 'Yo');
-    next();
+    req.log.info('Client requesting notifications for %s', req.params.username);
+    
+    storage.getNotificationsFor(req.params.username).then(function(notifications) {
+        res.send(200, notifications);
+    }).
+    catch(function(error) {
+        req.log.error({ err: error }, 'github-listener: an error occurred while retrieving notification from the database');
+    }).
+    finally(function() {
+        next();
+    }).done();
 }
 
 var Setup = function(params) {
